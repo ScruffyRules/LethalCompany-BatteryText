5a74bbd7bae60e72d6591de9cfe559e322d6e86b
ScruffyRules <5356779+ScruffyRules@users.noreply.github.com>
2024-01-12 13:54:51 +1030

    Initial Commit

---
+++ ScruffyRuffles.LC.BatteryText/BatteryTextPatches.cs
@@ -0,0 +1,97 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using System;
+using TMPro;
+using UnityEngine;
+
+namespace ScruffyRuffles.LC.BatteryText
+{
+    [HarmonyPatch]
+    class BatteryTextPatches
+    {
+        private static GameObject template;
+        public static TextMeshProUGUI[] batteryTexts = new TextMeshProUGUI[0];
+
+        [HarmonyPatch(typeof(HUDManager), "Update")]
+        [HarmonyPostfix]
+        public static void Update(HUDManager __instance)
+        {
+            //if (GameNetworkManager.Instance.localPlayerController != __instance) return;
+            for (int i = 0; i < GameNetworkManager.Instance.localPlayerController.ItemSlots.Length; i++)
+            {
+                if (i >= batteryTexts.Length)
+                {
+                    Array.Resize(ref batteryTexts, i + 1);
+                }
+                if (batteryTexts[i] == null)
+                {
+                    var parentObj = __instance.itemSlotIconFrames[i].rectTransform;
+                    var itemObj = GameObject.Instantiate(template, parentObj.parent);
+                    itemObj.SetActive(true);
+                    itemObj.transform.localScale = 0.5f * Vector3.one;
+                    batteryTexts[i] = itemObj.GetComponent<TextMeshProUGUI>();
+                    var rectTransform = batteryTexts[i].GetComponent<RectTransform>();
+
+                    // For some reason each Slot has a different rotation, so we have to do some offsetting from parent
+                    rectTransform.anchorMin = rectTransform.anchorMax = 0.5f * Vector3.one;
+                    rectTransform.pivot = Vector3.one;
+                    float halfSize = parentObj.sizeDelta.x * 0.5f;
+                    rectTransform.anchoredPosition = parentObj.anchoredPosition + new Vector2(halfSize, -halfSize);
+                    rectTransform.anchoredPosition += new Vector2(Plugin.config.offsetX, Plugin.config.offsetY);
+                    rectTransform.SetParent(parentObj, true);
+                }
+                GrabbableObject grabbableObject = GameNetworkManager.Instance.localPlayerController.ItemSlots[i];
+                if (grabbableObject != null)
+                {
+                    if (grabbableObject.itemProperties.requiresBattery)
+                    {
+                        batteryTexts[i].text = Mathf.Floor(grabbableObject.insertedBattery.charge * 100f).ToString() + "%";
+                        if (Plugin.config.showTimeRemaining)
+                        {
+                            batteryTexts[i].text += "\n" + TimeSpan.FromSeconds(grabbableObject.itemProperties.batteryUsage * grabbableObject.insertedBattery.charge).ToString(@"mm\:ss");
+                        }
+                    }
+                    else
+                    {
+                        batteryTexts[i].text = "";
+                    }
+                    //batteryTexts[i].text += "\n$" + grabbableObject.scrapValue;
+                    //float weight = grabbableObject.itemProperties.weight;
+                    //weight -= 1;
+                    //weight *= 100;
+                    //batteryTexts[i].text += "\n" + weight.ToString() + " lbs";
+                    //batteryTexts[i].text += "\n" + grabbableObject.itemProperties.isConductiveMetal + " Z";
+                }
+                else
+                {
+                    batteryTexts[i].text = "";
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(HUDManager), "Awake")]
+        [HarmonyPostfix]
+        public static void Awake(HUDManager __instance)
+        {
+            if (__instance.weightCounter != null)
+            {
+                GameObject itemSlot = new($"ItemSlotBatteryText")
+                {
+                    layer = LayerMask.NameToLayer("UI")
+                };
+                itemSlot.SetActive(false);
+
+                var tmpugui = itemSlot.AddComponent<TextMeshProUGUI>();
+                tmpugui.font = __instance.weightCounter.font;
+                tmpugui.fontSize = __instance.weightCounter.fontSize;
+                tmpugui.color = __instance.batteryIcon.color;
+                tmpugui.alignment = TextAlignmentOptions.TopRight;
+                tmpugui.enableAutoSizing = __instance.weightCounter.enableAutoSizing;
+                tmpugui.fontSizeMin = __instance.weightCounter.fontSizeMin;
+                tmpugui.fontSizeMax = __instance.weightCounter.fontSizeMax;
+
+                template = itemSlot;
+            }
+        }
+    }
+}
---
+++ ScruffyRuffles.LC.BatteryText/Plugin.cs
@@ -0,0 +1,90 @@
+﻿using BepInEx;
+using HarmonyLib;
+using BepInEx.Configuration;
+using System.IO;
+using System;
+using UnityEngine;
+
+namespace ScruffyRuffles.LC.BatteryText
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private const string modGUID = "ScruffyRuffles.BatteryText";
+        private const string modName = "BatteryText";
+        private const string modVersion = "1.0.0";
+
+        public static BepInEx.Logging.ManualLogSource PluginLogger;
+        public static ConfigA config;
+
+        private void Awake()
+        {
+            PluginLogger = Logger;
+            // Plugin startup logic
+            PluginLogger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            // Initialize Harmony
+            var harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+            harmony.PatchAll();
+
+            config = new ConfigA(this);
+        }
+
+        public class ConfigA
+        {
+            private readonly ConfigFile config;
+            private readonly FileSystemWatcher watcher;
+            private readonly ConfigEntry<bool> _showTimeRemaining;
+            private readonly ConfigEntry<float> _offsetX;
+            private readonly ConfigEntry<float> _offsetY;
+
+            public bool showTimeRemaining => _showTimeRemaining.Value;
+            public float offsetX => _offsetX.Value;
+            public float offsetY => _offsetY.Value;
+            public ConfigA(Plugin plugin)
+            {
+                config = new ConfigFile(Path.Combine(BepInEx.Paths.ConfigPath, modGUID + ".cfg"), saveOnInit: true, MetadataHelper.GetMetadata(plugin));
+
+                _showTimeRemaining = config.Bind("General", "Show Time Remaining", true, "Shows the remaining time under the percentage");
+                _offsetX = config.Bind("General", "Offset X", 0.0f, "Position Offset on the X Axis");
+                _offsetY = config.Bind("General", "Offset Y", -2.0f, "Position Offset on the Y Axis");
+
+                watcher = new FileSystemWatcher(Path.GetDirectoryName(config.ConfigFilePath), "*.cfg")
+                {
+                    NotifyFilter = NotifyFilters.Attributes
+                                 | NotifyFilters.CreationTime
+                                 | NotifyFilters.DirectoryName
+                                 | NotifyFilters.FileName
+                                 | NotifyFilters.LastAccess
+                                 | NotifyFilters.LastWrite
+                                 | NotifyFilters.Security
+                                 | NotifyFilters.Size
+                };
+
+                watcher.Changed += ConfigFileChanged;
+                watcher.EnableRaisingEvents = true;
+            }
+
+            private void OnConfigReloaded()
+            {
+                for (int i = 0; i < BatteryTextPatches.batteryTexts.Length; i++)
+                {
+                    var rectTransform = BatteryTextPatches.batteryTexts[i].GetComponent<RectTransform>();
+                    var parentObj = rectTransform.parent.GetComponent<RectTransform>();
+                    rectTransform.SetParent(parentObj.parent);
+                    rectTransform.anchoredPosition = parentObj.anchoredPosition + (new Vector2(parentObj.sizeDelta.x * 0.5f, -parentObj.sizeDelta.x * 0.5f) * parentObj.localScale);
+                    rectTransform.anchoredPosition += new Vector2(Plugin.config.offsetX, Plugin.config.offsetY) * parentObj.localScale;
+                    rectTransform.SetParent(parentObj, true);
+                }
+            }
+
+            private void ConfigFileChanged(object sender, FileSystemEventArgs e)
+            {
+                //PluginLogger.LogInfo($"{e.ChangeType} : {e.FullPath}");
+                if (e.FullPath != config.ConfigFilePath) return;
+                //PluginLogger.LogInfo("Config File Modified, Reloading!");
+                config.Reload();
+                OnConfigReloaded();
+            }
+        }
+    }
+}
